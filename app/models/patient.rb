0730b9015de178beec555b385eccca6bf9de0ee7
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-18 18:01:46 -0400

    update solution

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,4 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
-
-  # def time_format
-  #   self.strftime("%B %e, %Y at %k:%M")
-  # end
 end
