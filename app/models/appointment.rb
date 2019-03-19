70a1158e4c8deb8624d637603d334aa40b46b8e4
Sean <shonkelli@gmail.com>
2019-03-19 14:44:54 -0400

    add doctor#show page

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  def pretty_datetime
+    dt = self.appointment_datetime
+    dt.strftime('%B %d, %Y at %l:%M')
+  end
 end
